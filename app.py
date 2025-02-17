aea30ce140f79b8409b4c7cce7886a88c5d352c1
Juan R <130023488+9juanjuan7@users.noreply.github.com>
2025-02-17 06:32:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import atexit
 import os
 import glob
 
+app = create_app()
 # Function to delete all files in uploads folder when Flask stops
 def cleanup_uploads():
     files = glob.glob(os.path.join("website/uploads", "*"))  # Get all files in uploads/
@@ -15,5 +16,4 @@ def cleanup_uploads():
 atexit.register(cleanup_uploads)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
